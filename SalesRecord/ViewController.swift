d6c1dfca847d40cfc62c95361be50236487ff6fa
James Thang <jamesthang1996@gmail.com>
2020-05-27 09:14:45 +0700

    Initial Commit

---
+++ SalesRecord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SalesRecord
+//
+//  Created by James Thang on 5/27/20.
+//  Copyright © 2020 James Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
